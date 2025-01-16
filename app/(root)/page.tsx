74ce8d5d6fa928f0ba785ea66db0954390e37a08
ianmmworia <ianmworia156@gmail.com>
2025-01-17 00:01:58 +0300

    implement displaying real bank data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,23 @@
 import HeaderBox from "@/components/HeaderBox";
 import RightSideBar from "@/components/RightSideBar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
-const Home = async () => {
+const Home = async ({searchParams: {id, page}}: SearchParamProps) => {
 
   const loggedIn = await getLoggedInUser()
+
+  const accounts = await getAccounts({
+    userId: loggedIn.$id
+  })
+
+  if (!accounts) return
+
+  const appwriteItemId = ( id as string ) || accounts?.data[0]?.appwriteItemId 
+
+  const account = await getAccount ({appwriteItemId})
 
   return (
     <section className="home">
@@ -16,22 +27,23 @@ const Home = async () => {
           <HeaderBox 
           type = "greeting"
           title = "Welcome"
-          user = {loggedIn?.name || "Guest"}
+          user = {loggedIn?.firstName || "Guest"}
           subtext="Access and manage your account and transactions efficiently"
           />
 
           <TotalBalanceBox
-            accounts={[]}
-            totalBanks ={1}
-            totalCurrentBalance = {1250.35}
+            accounts={accounts?.data}
+            totalBanks ={accounts?.totalBanks}
+            totalCurrentBalance = {accounts?.totalCurrentBalance}
           />
         </header>
         RECENT TRANSACTIONS
       </div>
+
       <RightSideBar 
         user={loggedIn}
-        transactions={[]}
-        banks={[{currentBalance:10000},{currentBalance:11000}]}
+        transactions={accounts?.transactions}
+        banks={accounts?.data?.slice(0,2)}
       />
     </section>
   );
--- components/RightSideBar.tsx
+++ components/RightSideBar.tsx
@@ -11,14 +11,14 @@ const RightSideBar = ({ user, transactions, banks }: RightSidebarProps) => {
         <div className="profile">
           <div className="profile-img">
             <span className="text-4xl font-bold text-blue-500">
-              {user?.name[0]}
+              {user.firstName[0]}
             </span>
           </div>
           <div className="profile-details">
             <h1 className="profile-name">
-              {user?.name}
+              {user.firstName} {user.lastName}
             </h1>
-            <p className="profile-email">{user?.email}</p>
+            <p className="profile-email">{user.email}</p>
           </div>
         </div>
       </section>
@@ -38,7 +38,7 @@ const RightSideBar = ({ user, transactions, banks }: RightSidebarProps) => {
                 <BankCards 
                 key={banks[0].$id}
                 account={banks[0]}
-                userName = {`${user?.name}`}
+                userName = {`${user.firstName} ${user.lastName}`}
                 showBalance = {false}/>
                 </div>
             {banks[1] && (
@@ -46,7 +46,7 @@ const RightSideBar = ({ user, transactions, banks }: RightSidebarProps) => {
                  <BankCards 
                 key={banks[1].$id}
                 account={banks[1]}
-                userName = {`${user?.name}`}
+                userName = {`${user.firstName} ${user.lastName}`}
                 showBalance = {false}/>
               </div>
             )}
